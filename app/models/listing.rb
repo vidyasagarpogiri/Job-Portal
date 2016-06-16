def42cc3d2446c701edc117ac6a596160609d145
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-06-16 16:16:57 +0530

    Added validation
    Added validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,9 @@
 class Listing < ActiveRecord::Base
-	acts_as_votable
-	belongs_to :user
-	validates :title,:description,:experience,:address, presence: true
-	#has_attached_file :image, :styles => { :medium => "100x100>" }
-  	#validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-  	#has_attached_file :image1, :styles => { :medium => "100x100>" }
-  	#validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+acts_as_votable
+belongs_to :user
+validates :title,:description,:experience,:address, presence: true 
+  #has_attached_file :image, :styles => { :medium => "100x100>" }
+  #validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  #has_attached_file :image1, :styles => { :medium => "100x100>" }
+  #validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
