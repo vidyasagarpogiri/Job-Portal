563068e33ce3c612155d6555c03f9fef83a81a98
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-06-17 18:36:39 +0530

    Updated Schema
    Updated Schema

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150319114954) do
     t.string   "name"
   end
 
+
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
