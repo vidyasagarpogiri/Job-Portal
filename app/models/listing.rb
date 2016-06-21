e7507f43d3480959bb985ceb24d40571939dbb3c
Vidya Sagar Pogiri <vidyasagarpogiri@users.noreply.github.com>
2016-06-21 16:17:29 +0530

    Updated model validation
    Updated model validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,7 @@
 class Listing < ActiveRecord::Base
 acts_as_votable
 belongs_to :user 
-validates :title,:description,:experience,:address, presence: true  #validation added to title
+validates :title,:description,:experience,:address, presence: true  # validation added to title
   #has_attached_file :image, :styles => { :medium => "100x100>" }
   #validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   #has_attached_file :image1, :styles => { :medium => "100x100>" }
